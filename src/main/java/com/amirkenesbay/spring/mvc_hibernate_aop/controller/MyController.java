b7de206623fa2786807778c6aa91d3e28aecf8c4
Amir <amirkenesbay@gmail.com>
2022-01-12 04:08:46 +0600

    Initial commit

---
+++ src/main/java/com/amirkenesbay/spring/mvc_hibernate_aop/controller/MyController.java
@@ -0,0 +1,24 @@
+package com.amirkenesbay.spring.mvc_hibernate_aop.controller;
+
+import com.amirkenesbay.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.amirkenesbay.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @RequestMapping("/")
+    public String showAllEmployees(Model model){
+        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-employees";
+    }
+}
---
+++ src/main/java/com/amirkenesbay/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package com.amirkenesbay.spring.mvc_hibernate_aop.dao;
+
+import com.amirkenesbay.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
